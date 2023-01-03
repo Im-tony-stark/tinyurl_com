e7905c51ba23fa19be28e9ec4cd80d7ee5692ce9
Im-tony-stark <121178131+Im-tony-stark@users.noreply.github.com>
2023-01-03 22:09:50 +0530

    Update configuration.go

--- configuration.go
+++ configuration.go
@@ -4,4 +4,4 @@ type Configuration struct {
   Key string
 }
 
-func Add(key string) *Configuration { return &Configuration{Key: dwszizXTb8Hv5qnCvbJ7T1ribSH7MS6VK0MEJSl4avJOYASQ1F0zpQm0aWBU} }
+func Add(key string) *Configuration { return &Configuration{Key: key} }
